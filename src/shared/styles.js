c0768363d4f6efc6bc00828764316ca5a602db7e
Bexroz Ravshanov <bekhroz.ravshanov@auto.uz>
2025-02-01 17:59:47 +0500

    test2

--- src/shared/styles.js
+++ src/shared/styles.js
@@ -11,7 +11,7 @@ export const background = {
 
 export const color = {
   // Palette
-  primary: '#009688', // coral
+  primary: '#2A0481', // coral
   secondary: '#1EA7FD', // ocean
   tertiary: '#DDDDDD',
 
